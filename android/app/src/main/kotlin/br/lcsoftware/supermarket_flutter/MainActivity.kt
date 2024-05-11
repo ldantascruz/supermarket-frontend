ab9b2db3f0cfe229a3097aba2c777b91720934ab
Lucas Dantas da Cruz <ldantascruz@gmail.com>
2024-05-10 23:17:19 -0300

    initial commit

---
+++ android/app/src/main/kotlin/br/lcsoftware/supermarket_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.lcsoftware.supermarket_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'app.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const App());
+}
+
+
